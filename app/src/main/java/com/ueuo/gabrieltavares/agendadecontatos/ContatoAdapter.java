ba9c89fa3c23a13fab6e3926c92f6eada0ca3caa
GabrielTavares99 <gabriel.tavares99@hotmail.com>
2017-10-07 23:07:24 -0300

    Trocando layout para exibicao da foto do contato salvo

--- app/src/main/java/com/ueuo/gabrieltavares/agendadecontatos/ContatoAdapter.java
+++ app/src/main/java/com/ueuo/gabrieltavares/agendadecontatos/ContatoAdapter.java
@@ -1,10 +1,14 @@
 package com.ueuo.gabrieltavares.agendadecontatos;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.text.InputFilter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ueuo.gabrieltavares.agendadecontatos.dominio.entidades.Contato;
@@ -42,9 +46,37 @@ public class ContatoAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(context);
-        textView.setText(contatos.get(position).getId()+""+contatos.get(position).getNome());
-        return textView;
+        Contato contato = contatos.get(position);
+
+//        A CONVERTVIEW é uma view já instanciada que está sendo reaproveitada
+       View view = convertView;
+
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        if (view == null){
+//            Parâmetros
+//                    O layout a ser inflado
+//                    O item pai
+//                    False para dizer que a view inflada não deve ser colocada dentro do item pai
+            view = layoutInflater.inflate(R.layout.activity_act_linha_contato_foto, parent ,false);
+        }
+        TextView nome = (TextView) view.findViewById(R.id.lbl_nome);
+        nome.setText(contato.getNome());
+
+        TextView telefone = (TextView) view.findViewById(R.id.lbl_telefone);
+        telefone.setText(contato.getTelefone());
+
+        ImageView foto = (ImageView) view.findViewById(R.id.layout_foto_img_foto) ;
+
+        String caminhoFoto = contato.getCaminhoFoto();
+        if (caminhoFoto != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 64, 64, true);
+            foto.setImageBitmap(bitmapReduzido);
+            foto.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }
+
+        return view;
     }
 
 
